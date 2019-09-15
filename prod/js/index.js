28140f2b64e2f4078d2e5a535ac43713d2179780
Dan Levy <dlevy1@kaleidahealth.org>
2019-09-15 19:52:40 -0400

    Added tablet media queries for About Me page. Re-wrote the underlying structure for the navigation bar.

--- prod/js/index.js
+++ prod/js/index.js
@@ -10,30 +10,5 @@
  * Initial function when page loads
  */
 document.addEventListener('DOMContentLoaded', () => {
-    hamburgerMenuShowHide()
+
 })
-
-/**
- * Shows or hides the header navigation when the hamburger menu is clicked
- */
-function hamburgerMenuShowHide () {
-    // Hamburger menu elements
-    const hamburgerMenu = document.querySelector('#header-hamburger-menu')
-    const headerNavigation = document.querySelector('#header-nav')
-    const header = document.querySelector('header')
-
-    hamburgerMenu.addEventListener('click', () => {
-        // If navigation is hidden, show it; otherwise, hide it
-        if (getComputedStyle(headerNavigation).display === 'none') {
-            header.style.height = '234px'
-            header.style.gridTemplateRows = '50px 1fr'
-            setTimeout(() => {
-                headerNavigation.style.display = 'block'
-            }, 200)
-        } else {
-            header.style.height = '50px'
-            header.style.gridTemplateRows = '50px'
-            headerNavigation.style.display = 'none'
-        }
-    })
-}
