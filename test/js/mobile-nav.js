ac12dbb3be15d83b88804911673603d940649c65
Dan Levy <danlevy124@gmail.com>
2020-01-13 21:37:46 -0500

    Website redesign.

--- test/js/mobile-nav.js
+++ test/js/mobile-nav.js
@@ -9,26 +9,46 @@
 /**
  * Initial function when page loads
  */
-document.addEventListener('DOMContentLoaded', () => {
-    showHideMobileNav()
-})
+document.addEventListener("DOMContentLoaded", () => {
+    showHideMobileNav();
+    onResize();
+});
 
 /**
  * Shows or hides the mobile navigation when the hamburger menu is clicked
  */
-function showHideMobileNav () {
-    const headerHamburgerMenu = document.querySelector('#header-hamburger-menu')
-    const headerNav = document.querySelector('#header-nav')
-    const headerNavLinks = headerNav.children
+function showHideMobileNav() {
+    const headerHamburgerMenu = document.querySelector("#nav-bar-hamburger-menu");
+    const headerNav = document.querySelector("#main-nav");
+    console.log(headerNav);
+    const headerNavLinks = headerNav.children;
 
-    headerHamburgerMenu.addEventListener('click', () => {
-        headerNav.classList.toggle('header-nav-show')
-        if (headerNav.classList.contains('header-nav-show')) {
+    headerHamburgerMenu.addEventListener("click", () => {
+        headerNav.classList.toggle("nav-bar-nav-show");
+        if (headerNav.classList.contains("nav-bar-nav-show")) {
             setTimeout(() => {
-                showHideMobileNavLinks(headerNavLinks)
-            }, 250)
+                showHideMobileNavLinks(headerNavLinks);
+            }, 250);
         } else {
-            showHideMobileNavLinks(headerNavLinks)
+            showHideMobileNavLinks(headerNavLinks);
+        }
+    });
+}
+
+/**
+ * Hides the mobile navigation when the screen becomes too large
+ */
+function onResize() {
+    const headerNav = document.querySelector("#main-nav");
+    let isMobile = window.innerWidth < 768 ? true : false;
+
+    window.addEventListener('resize', () => {
+        if (isMobile && window.innerWidth >= 768) {
+            headerNav.classList.remove("nav-bar-nav-show");
+            Array.from(headerNav.children).forEach(link => {
+                link.classList.remove("main-nav-nav-item-show");
+            });
+            isMobile = false;
         }
     })
 }
@@ -37,8 +57,8 @@ function showHideMobileNav () {
  * Shows or hides the mobile navigation links when the hamburger menu is clciked
  * @param {*} links Mobile navigation links
  */
-function showHideMobileNavLinks (links) {
+function showHideMobileNavLinks(links) {
     Array.from(links).forEach(link => {
-        link.classList.toggle('header-nav-item-show')
-    })
+        link.classList.toggle("main-nav-nav-item-show");
+    });
 }
