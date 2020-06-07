35dcbd6f7132401511160efbb14694d614b90581
Dan Levy <danlevy124@gmail.com>
2020-06-07 13:10:06 -0400

    Added Home Page Image Imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,24 @@
 // JS imports
 import "../js/header";
 
+// File imports
+import "../assets/files/daniel-levy-resume.pdf";
+
+// Icon imports
+import "../assets/icons/email-icon.svg";
+
+// Image imports
+import "../assets/images/about-me.png";
+
 // Logo imports
 import "../assets/logos/github-logo.svg";
 import "../assets/logos/linkedin-logo.svg";
 import "../assets/logos/qualtrics-logo.svg";
 import "../assets/logos/kaleida-health-logo.svg";
-
-// Icon imports
-import "../assets/icons/email-icon.svg";
-
-// File imports
-import "../assets/files/daniel-levy-resume.pdf";
+import "../assets/logos/florida-tech-logo.jpg";
+import "../assets/logos/treehouse-logo.svg";
+import "../assets/logos/udemy-logo.svg";
+import "../assets/logos/frontend-masters-logo.svg";
 
 // Style imports
 import "../scss/styles.scss";
